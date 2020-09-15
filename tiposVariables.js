0c58100e137a7589eadae2661b9e0c229386c25b
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-14 21:09:22 -0500

    tiposVariables

---
+++ tiposVariables.js
@@ -0,0 +1,31 @@
+var nombre;
+var cadena = 'Mao';
+var numero = 12;
+var boolean = true;
+var esNull = null;
+
+var persona = { nombre: 'pedro', apellido: 'perez', edad: 23 };
+var array = [ 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo' ];
+var array1 = [ 32, 45, 21,55 ];
+var array2 = [ 54, true, 'uno', { nombre: 'pedro', apellido: 'perez', edad: 23 }, ['uno', 'dos']];
+var arrayTrabajadores = [
+    { nombre: 'pedro', apellido: 'perez', edad: 23 },
+    { nombre: 'mauricio', apellido: 'pulgarin', edad: 38 },
+    { nombre: 'maria', apellido: 'suarez', edad: 23 },
+];
+
+// console.log(typeof(nombre));
+// console.log(typeof(cadena));
+// console.log(typeof(numero));
+// console.log(typeof(boolean));
+// console.log(typeof(esNull));
+// console.log(persona);
+// console.log(array);
+// console.log(array1);
+// console.log(array2);
+console.log(arrayTrabajadores[0].nombre);
+console.log(array2[4][1]);
+
+
+
+
