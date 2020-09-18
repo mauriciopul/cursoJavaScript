af28ee203d84ae455a171967276ad4fc12b48795
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 19:45:01 -0500

    Ejercicio 7 Resuelto

--- algoritmos1/ejercicio7.js
+++ algoritmos1/ejercicio7.js
@@ -1,8 +1,19 @@
 /*
 7. Leer dos números enteros, restar el menor del mayor y mostrar el resultado
 */
+function isHigher(nH1, nH2) {
+    return nH1 > nH2 ? nH1 : nH2;
+};
 
+function isLess(nL1, nL2) {
+    return nL1 < nL2 ? nL1 : nL2;
+};
 
-
+function subtract(num1, num2) {
+    return isHigher(num1, num2) - isLess(num1, num2);
+};
+// console.log(isLess(7, 11));
+// console.log(isHigher(7, 11));
+console.log(subtract(7, 11));
 
 
