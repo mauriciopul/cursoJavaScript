6d0c5f2c617cdd287669fc3a7ff2dba92adb12c8
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 23:15:45 -0500

    Algoritmos2 - Ejercicio 6 Resuelto

--- algoritmos2/algoritmo (6).js
+++ algoritmos2/algoritmo (6).js
@@ -1,3 +1,18 @@
 /**
- * 6. Solicitar un entero y determinar sí es múltiplo de 3 y además que se encuentre en el rango (100-200)
- */
+* 6. Solicitar un entero y determinar sí es múltiplo de 3 y además que se encuentre en 
+*      el rango (100-200)
+*/
+
+function esMultiplo(num){
+    return num % 3 === 0 ? true: false;
+};
+
+function enRango(num){
+    if(num >=100 && num <=200){
+        return esMultiplo(num);
+    }else{
+        return false;
+    };
+};
+console.log(enRango(198));
+
