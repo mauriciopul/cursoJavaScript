5f4da3c45487d5430ea443e30f5a1db258385ee7
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 22:25:21 -0500

    Ejercicio 10 Resuelto

--- algoritmos1/ejercicio10.js
+++ algoritmos1/ejercicio10.js
@@ -3,8 +3,31 @@
     indicando la edad y nombre del mayor y cuantos años de diferencia tiene con el menor.
  */
 
+//Se obtiene el mayor
+function isHigher(edad1, edad2) {
+    return edad1 > edad2 ? true : false;
+};
 
+//Se obtiene la diferencia(numero positivo) entre 2 números
+function subtract(n1, n2) {
+    return n1 > n2 ? n1 - n2 : n2 - n1;
+};
 
+function edadHermanos(herm1Nom, herm1Ed, herm2Nom, herm2Ed) {
+
+    let diferencia = subtract(herm1Ed, herm2Ed);
+    let hermano1 = { nombre: herm1Nom, edad: herm1Ed };
+    let hermano2 = { nombre: herm2Nom, edad: herm2Ed };
+    let mayor;
+
+    //A la variable 'mayor' se le asigna el dato del hermano mayor 
+    mayor = isHigher(herm1Ed, herm2Ed) ? hermano1 : hermano2;
+
+    return `
+    Nombre del mayor: ${mayor.nombre} - Edad: ${mayor.edad} años
+    Diferencia de edad: ${diferencia} años`;
+};
+console.log(edadHermanos('Juan', 25, 'Pedro', 48));
 
 
 
