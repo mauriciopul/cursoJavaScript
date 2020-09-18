e0e70f8f134bd21b983b8a30856f62ca9fd93632
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 20:15:03 -0500

    Ejercicio 8 Resuelto

--- algoritmos1/ejercicio8.js
+++ algoritmos1/ejercicio8.js
@@ -2,8 +2,15 @@
  8. Leer dos números si el primero es mayor mostrar la Resta de los dos números, por otro 
     lado si el segundo es mayor mostrar la suma de ambos.
  */
+function isHigher(nO1, nO2) {
+    return nO1 > nO2 ? true : false;
+}
+function showAddOrSubtract(num1, num2) {
+    if (num1 === num2) {
+        return 'Los números ingresados son iguales';
+    } else {
+        return isHigher(num1, num2) ? num1 - num2 : num1 + num2;
+    }
+};
+console.log(showAddOrSubtract(6, 5));
 
-
-
-
- 
