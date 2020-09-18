dd500e775704c6c3ddf6a12028f6e31ed24078c9
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 22:59:07 -0500

    Algoritmos2 - Ejercicio 3 Resuelto

--- algoritmos2/algoritmo (3).js
+++ algoritmos2/algoritmo (3).js
@@ -1,3 +1,13 @@
 /**
- * 3. Dados 2 valores de entrada imprima siempre la división del mayor entre el menor.
- */
+3. Dados 2 valores de entrada imprima siempre la división del mayor entre el menor.
+*/
+
+function isHigher(n1, n2) {
+    return n1 > n2 ? true : false;
+}
+
+function divMayor(n1, n2) {
+    return isHigher(n1, n2) ? n1 / n2 : n2 / n1;
+}
+
+console.log(divMayor(12, 36));
