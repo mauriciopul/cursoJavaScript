f960c2abad547688aeb92f55a468f7cd05e71772
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 23:04:35 -0500

    Algoritmos2 - Ejercicio 5 Resuelto

--- algoritmos2/algoritmo (5).js
+++ algoritmos2/algoritmo (5).js
@@ -1,3 +1,8 @@
 /**
  * 5. Elabora un algoritmo para leer un número y determinar si es par o impar.
- */
+*/
+
+function esPar(num) {
+    return num % 2 === 0 ? true : false;
+}
+console.log(esPar(0));
