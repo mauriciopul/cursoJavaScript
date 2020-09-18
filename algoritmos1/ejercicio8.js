487f81767e2c0006e9186049ba4ed2da6f7da0c7
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 20:21:43 -0500

    Ejercicio 9 Resuelto

--- algoritmos1/ejercicio8.js
+++ algoritmos1/ejercicio8.js
@@ -2,15 +2,15 @@
  8. Leer dos números si el primero es mayor mostrar la Resta de los dos números, por otro 
     lado si el segundo es mayor mostrar la suma de ambos.
  */
-function isHigher(nO1, nO2) {
-    return nO1 > nO2 ? true : false;
+function isHigher(num1, num2) {
+    return num1 > num2 ? true : false;
 }
-function showAddOrSubtract(num1, num2) {
+function sumarORestar(num1, num2) {
     if (num1 === num2) {
         return 'Los números ingresados son iguales';
     } else {
         return isHigher(num1, num2) ? num1 - num2 : num1 + num2;
     }
 };
-console.log(showAddOrSubtract(6, 5));
+console.log(sumarORestar(5, 5));
 
--- algoritmos1/ejercicio9.js
+++ algoritmos1/ejercicio9.js
@@ -2,6 +2,48 @@
  9. Diseñe un algoritmo que lea tres números y los imprima de mayor a menor
  */
 
+function isHigher(n1, n2) {
+    return n1 > n2 ? true : false;
+};
 
+function isLess(n1, n2) {
+    return n1 < n2 ? true : false;
+};
+
+function isMidleNumber(n1, n2, n3) {
+
+    let igual = n1 === n2 || n1 === n3 || n2 === n3;
+
+    if (igual)
+        return 'Hay 2 números repetidos';
+
+    if ((isLess(n1, n2) && isHigher(n1, n3)) || (isHigher(n1, n2) && isLess(n1, n3)))
+        return n1;
+
+    if ((isLess(n2, n1) && isHigher(n2, n3)) || (isHigher(n2, n1) && isLess(n2, n3)))
+        return n2
+
+    if ((isLess(n3, n1) && isHigher(n3, n2)) || (isHigher(n3, n1) && isLess(n3, n2)))
+        return n3;
+};
+
+function numberPositions(n1, n2, n3) {
+    let numMenor, numIntermedio, numMayor;
+    if (isLess(n1, n2) && isLess(n1, n3)) { numMenor = n1 };
+    if (isLess(n2, n1) && isLess(n2, n3)) { numMenor = n2 };
+    if (isLess(n3, n1) && isLess(n3, n2)) { numMenor = n3 };
+
+    if (isHigher(n2, n1) && isHigher(n2, n3)) { numMayor = n2 };
+    if (isHigher(n1, n2) && isHigher(n1, n3)) { numMayor = n1 };
+    if (isHigher(n3, n1) && isHigher(n3, n2)) { numMayor = n3 };
+
+    numIntermedio = isMidleNumber(n1, n2, n3);
+    return {
+        mayor: numMayor,
+        intermedio: numIntermedio,
+        menor: numMenor
+    };
+}
+console.log(numberPositions(8, 9, 6));
 
 
