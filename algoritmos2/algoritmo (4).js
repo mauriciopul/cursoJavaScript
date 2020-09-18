9c7d0366b1d552254a669b37296caecf5684f981
mauricio pulgarin <mauriciopul@gmail.com>
2020-09-17 23:02:30 -0500

    Algoritmos2 - Ejercicio 4 Resuelto

--- algoritmos2/algoritmo (4).js
+++ algoritmos2/algoritmo (4).js
@@ -1,3 +1,12 @@
 /**
  * 4. Calcular la media (promedio) de 3 números.
- */
+*/
+
+function promedio(n1, n2, n3){
+    return (n1+n2+n3) / arguments.length;
+}
+console.log(promedio(2,4,12));
+
+
+
+
